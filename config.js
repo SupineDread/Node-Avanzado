aafe0aa8fa926a29eab55b9736bb9db88b471c2c
SupineDread <hazielfe@gmail.com>
2018-05-19 11:10:36 -0500

    l

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: process.env.PORT || 8081,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/zoo',
+  db: process.env.MONGODB || 'mongodb://admin:123@ds014388.mlab.com:14388/webtask-test',
 }
